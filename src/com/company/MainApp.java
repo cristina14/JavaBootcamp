300c4d9d650360334a7f125f960f6329bb5fe8ad
unknown <ularucristina@yahoo.com>
2018-04-18 20:09:53 +0300

    Third version

--- src/com/company/MainApp.java
+++ src/com/company/MainApp.java
@@ -5,15 +5,20 @@ public class MainApp
 
     public static void main(String[] args) {
 
-       final Car joesCar = new Car(5,200000,true, "auto");
-       final Bus steveBus = new Bus(3,100000,true,31);
-        final Tipper petersTipper= new Tipper(6, 80000,false,15);
+       final Vehicle joesCar = new Car(5,200000,true, "auto");
+       final Vehicle steveBus = new Bus(3,100000,true,31);
+        final  Vehicle petersTipper= new Tipper(6, 80000,false,15);
 
         final InsurancePolicyCalculator calculator = InsurancePolicyCalculator.INSTANCE;
 
-        final int joesInsurancePolicyCost = calculator.calculate(joesCar);
-        final int stevesInsurancePolicyCost = calculator.calculate(steveBus);
-        final int petersInsurancePolicyCost = calculator.calculate(petersTipper);
+        final Formula carBasicFormula = new CarBasicFormula();
+        final Formula busBasicFormula = new BusBasicFormula();
+        final Formula tipperBasicFormula = new TipperBasicFormula();
+
+
+        final int joesInsurancePolicyCost = calculator.calculate(joesCar,carBasicFormula);
+        final int stevesInsurancePolicyCost = calculator.calculate(steveBus,busBasicFormula);
+        final int petersInsurancePolicyCost = calculator.calculate(petersTipper,tipperBasicFormula);
 
 
         System.out.println("Joe's policy cost is: "+ joesInsurancePolicyCost);
---
+++ src/com/company/TipperBasicFormula.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TipperBasicFormula implements Formula {
+
+    public int calculate(Vehicle vehicle) {
+        int cost = 300 * vehicle.getAge();
+        cost += vehicle.getNumberOfMiles() > 80000 ? 700 : 0;
+        return cost;
+    }
+}
